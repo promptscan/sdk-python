75d2d4f29b2958ee1f8f103cd818f491628265a8
Bogdan Sulima <bogdan.sulima@gmail.com>
2024-11-28 11:00:58 +0100

    Adding missing httpx dependency

--- src/promptscan_sdk/__init__.py
+++ src/promptscan_sdk/__init__.py
@@ -1,4 +1,4 @@
 from .sdk import PromptScanSDK, configure, collect, client, close
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["PromptScanSDK", "configure", "collect", "client", "close"]
